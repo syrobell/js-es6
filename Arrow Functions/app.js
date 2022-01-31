3c1788d7a75df344b627845316f2f913e62f994a
Adnan Dedeoğlu <ado_adnen@hotmail.com>
2022-01-31 23:18:07 +0300

    update

---
+++ Arrow Functions/app.js
@@ -0,0 +1,21 @@
+// const merhaba = function(){
+
+
+// }
+
+// merhaba();
+
+const merhaba = () =>{
+    console.log("Merhaba");
+}
+
+merhaba();
+
+const merhaba2 = (isim,soyisim) => console.log("Merhaba", isim, soyisim);
+
+
+merhaba2("Adnan","Dedeoğlu");
+
+const cube = x => x*x*x;
+
+console.log(cube(4))
---
+++ Destructing/app.js
@@ -0,0 +1,15 @@
+
+
+arr = [100,200,300,400];
+
+// number1 = arr[0];
+// number2 = arr[1];
+
+//Destructing Arrayin en başından başlayarak elemanları veriyor.
+
+// [number1,number2] = arr;
+
+const [number1,number2] = arr;
+
+
+console.log(number1,number2);
